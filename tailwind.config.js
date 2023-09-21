8d0260daaa103a50ca7c9f600b924af2f2e074b4
Haein Cho <haein.cho@3i.ai>
2023-09-21 14:56:14 +0900

    💄 font family updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      pretendard: ["Pretendard", "sans-serif"],
+    },
   },
   plugins: [],
 };
