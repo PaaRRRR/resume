14a051b6dbab8e57fb2c4fdc5c9734eed8893c50
Haein Cho <haein.cho@3i.ai>
2023-09-22 16:50:33 +0900

    💄 update info part

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "line-seperator": "radial-gradient(closest-side, #d2d2d7, transparent)",
+      },
+    },
     fontFamily: {
       pretendard: ["Pretendard", "sans-serif"],
     },
