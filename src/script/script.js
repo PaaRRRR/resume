795fa1c396ec43f15bcf4293080af269a2d13288
Haein Cho <haein.cho@3i.ai>
2023-09-22 18:59:35 +0900

    💄 add hero icons

---
+++ src/script/script.js
@@ -0,0 +1,4 @@
+const wrapper = document.querySelector(".swiper-wrapper");
+const items = [...wrapper.querySelectorAll(".swiper-item")];
+
+console.log("???", items);
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ export default {
     extend: {
       backgroundImage: {
         "line-seperator": "radial-gradient(closest-side, #d2d2d7, transparent)",
+        "hero-logo":
+          "linear-gradient(90deg,#fff,hsla(0,0%,100%,0) 20%,hsla(0,0%,100%,0) 80%,#fff)",
       },
     },
     fontFamily: {
