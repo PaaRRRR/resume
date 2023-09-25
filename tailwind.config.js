9d0a94eb982e7d59e5ac1bf646cbc5cd4834540d
Haein Cho <haein.cho@3i.ai>
2023-09-25 15:32:19 +0900

    💄 color updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,7 @@
 export default {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "line-seperator": "radial-gradient(closest-side, #d2d2d7, transparent)",
-        "hero-logo":
-          "linear-gradient(90deg,#fff,hsla(0,0%,100%,0) 20%,hsla(0,0%,100%,0) 80%,#fff)",
-      },
-    },
+    extend: {},
     fontFamily: {
       pretendard: ["Pretendard", "sans-serif"],
     },
