96b5f13950afba89f152a42b559f820eef6ef8c1
Haein Cho <haein.cho@3i.ai>
2023-10-05 17:36:26 +0900

    💄 update intro animation

---
+++ src/script/takeover.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    document.body.style.overflow = "unset";
+    document.querySelector("nav").style.display = "block";
+  }, 3000);
+});
