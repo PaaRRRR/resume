78b36de388b26bddd345e713081a4cd25330136a
Haein Cho <haein.cho@3i.ai>
2023-10-06 18:59:25 +0900

    💄 add renderImg, renderText animation

--- src/script/takeover.js
+++ src/script/takeover.js
@@ -34,16 +34,78 @@ function render() {
   ctx.clearRect(0, 0, sizes.width, sizes.height);
 }
 
-function renderText() {
+function renderImg(img) {
   ctx.clearRect(0, 0, sizes.width, sizes.height);
+  ctx.drawImage(img, 0, 0, sizes.width, sizes.height);
+}
 
-  ctx.fillStyle = "#FCEB57";
-  ctx.strokeStyle = "white";
-  ctx.font = "300px DharmaGothicC-HeavyItalic";
+function renderText(percentage) {
+  // ctx.clearRect(0, 0, sizes.width, sizes.height);
+
+  const word = "TAKE OVER";
+  const seconds = Math.ceil(percentage);
+
+  console.log("hello?", percentage);
+
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
 
-  ctx.fillText("TAKE OVER", sizes.width / 2, sizes.height / 2);
+  if (percentage <= 9) {
+    const currentText = word.slice(0, seconds);
+    strokeText(currentText, "300px", "#FCEB57", true);
+  } else {
+    if (seconds % 2 == 0) {
+      fillText(word, `${300 + 300 * (seconds / 20)}px`);
+    } else {
+      ctx.clearRect(0, 0, sizes.width, sizes.height);
+    }
+  }
+}
+
+function fillText(
+  text = "TAKE OVER",
+  size = "300px",
+  color = "#FCEB57",
+  reverse = false
+) {
+  ctx.clearRect(0, 0, sizes.width, sizes.height);
+  ctx.font = `${size} DharmaGothicC-HeavyItalic`;
+  ctx.fillStyle = color;
+  let xPos = sizes.width / 2;
+  let yPos = sizes.height / 2;
+
+  if (reverse) {
+    ctx.translate(0, 0);
+    ctx.rotate(Math.PI);
+
+    xPos = 0 - sizes.width / 2;
+    yPos = 0 - sizes.height / 2;
+  }
+
+  ctx.fillText(text, xPos, yPos);
+}
+
+function strokeText(
+  text = "TAKE OVER",
+  size = "300px",
+  color = "#FCEB57",
+  reverse = false
+) {
+  ctx.clearRect(0, 0, sizes.width, sizes.height);
+  ctx.font = `${size} DharmaGothicC-HeavyItalic`;
+  ctx.strokeStyle = color;
+  let xPos = sizes.width / 2;
+  let yPos = sizes.height / 2;
+
+  if (reverse) {
+    ctx.translate(0, 0);
+    ctx.rotate(Math.PI);
+
+    xPos = 0 - sizes.width / 2;
+    yPos = 0 - sizes.height / 2;
+  }
+
+  ctx.strokeText(text, xPos, yPos);
 }
 
 function renderRect(percentage) {
@@ -63,12 +125,37 @@ function renderRect(percentage) {
 function init() {
   resize();
 
+  // load Image
+  // const img = new Image();
+  // img.src = "../images/takeover/total_frame.jpg";
+  // img.onload = () => renderImg(img);
+
+  // black rect bar
+  // renderRect();
+
+  // load font
+  // show Take Over sequentially
+  const myFont = new FontFace(
+    "DharmaGothicC-HeavyItalic",
+    "url('../fonts/DharmaGothicC-HeavyItalic.ttf')"
+  );
+
+  myFont.load().then((font) => {
+    document.fonts.add(font);
+
+    console.log("Font loaded");
+
+    // strokeText();
+
+    tick();
+  });
+
   // const canvas = document.createElement("canvas");
   // const offscreen = canvas.transferControlToOffscreen();
   // const worker = new Worker("/script/worker.js");
   // worker.postMessage({ canvas: offscreen }, [offscreen]);
 
-  tick();
+  // tick();
 }
 
 function highResolution() {
@@ -98,8 +185,16 @@ function tick(fps) {
   requestAnimationCurrent = (fps - requestAnimationTimer) / 1000;
 
   // controll timer
-  if (!fps || requestAnimationCurrent * 10 <= 110) {
-    renderRect(requestAnimationCurrent * 10);
+
+  // renderRect
+  // if (!fps || requestAnimationCurrent * 10 <= 110) {
+  //   renderRect(requestAnimationCurrent * 10);
+  //   window.requestAnimationFrame(tick);
+  // }
+
+  // renderText
+  if (!fps || requestAnimationCurrent <= 20) {
+    renderText(requestAnimationCurrent);
     window.requestAnimationFrame(tick);
   }
 }
