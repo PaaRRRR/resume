56f07867465b560587ed6e499c81a7bb42df0b86
hihi1647 <hihi1647@gmail.com>
2024-02-01 22:55:36 +0700

    🌐 add localization(KO)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
 const isCodeSandbox =
   "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
 
-export default {
+export default defineConfig({
   root: "src/",
   publicDir: "../static/",
   base: "/resume/",
@@ -13,5 +17,11 @@ export default {
     outDir: "../dist",
     emptyOutDir: true,
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./src/index.html"),
+        ko: resolve(__dirname, "./src/ko/index.html"),
+      },
+    },
   },
-};
+});
