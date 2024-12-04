f06d417a14bfbc24ff3dc0a999ebce35df313f2d
hihi1647 <hihi1647@gmail.com>
2024-12-04 17:05:53 +0900

    👷 update path

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,13 @@ const isCodeSandbox =
   "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
 
 export default defineConfig({
-  root: "src/",
-  publicDir: "../static/",
-  base: "/resume/",
+  base: "/",
   server: {
     host: true,
     open: !isCodeSandbox, // Open if it's not a CodeSandbox
   },
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
